989397ced9d03e46a5a204adc594f9a318230e30
Rutuja das <rutuja20062000@gmail..com>
2025-07-06 00:56:18 +0530

    Added settimeout file

---
+++ settimeout.js
@@ -0,0 +1,17 @@
+//settimeout takes any argument as js code 
+//it will find in the code where this hii variable exists and then it will give error
+
+// const hii = "hello"
+// setTimeout("hii") // trating hii as a variable
+
+setTimeout("console.log(true)") // taking this code printing true in the console
+//we can tell Settimeout after how many sec we are printing the code
+// fitst param - js code
+// 2nd param - time
+
+setTimeout("console.log('rutu')",2000)
+
+// return value 
+//returns id of timer
+//with the help of clearTimeout we are clearing the setitmeout and we can pass the id in the  argument
+ 
